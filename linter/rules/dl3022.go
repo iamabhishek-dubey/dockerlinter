057e71210eb84db8e7b2e3ac33842681ede5907d
iamabhishek-dubey <abhishekbhardwaj510@gmail.com>
2019-05-04 16:04:41 +0530

    Added 23rd rule

---
+++ linter/rules/dl3022.go
@@ -0,0 +1,34 @@
+package rules
+
+import (
+	"strings"
+
+	"github.com/moby/buildkit/frontend/dockerfile/parser"
+)
+
+// validateDL3022 COPY --from should reference a previously defined FROM alias
+func validateDL3022(node *parser.Node) (rst []ValidateResult, err error) {
+	fromImage := ""
+	isAs, isAsBuild := false, false
+	for _, child := range node.Children {
+		if child.Value == FROM {
+			for _, v := range strings.Fields(child.Original) {
+				switch v {
+				case "as":
+					isAs = true
+				case "build":
+					if isAs {
+						isAsBuild = true
+					}
+				default:
+					if fromImage == "" && v != "FROM" && v != "from" {
+						fromImage = v
+					} else if fromImage == v && !isAsBuild {
+						rst = append(rst, ValidateResult{line: child.StartLine, addMsg: ""})
+					}
+				}
+			}
+		}
+	}
+	return rst, nil
+}
