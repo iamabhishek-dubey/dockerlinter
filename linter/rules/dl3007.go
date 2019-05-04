0a26722bce732582c99da39f0c08a7f7b3093a8d
iamabhishek-dubey <abhishekbhardwaj510@gmail.com>
2019-05-04 15:49:39 +0530

    Added eighth rule

---
+++ linter/rules/dl3007.go
@@ -0,0 +1,19 @@
+package rules
+
+import (
+	"regexp"
+
+	"github.com/moby/buildkit/frontend/dockerfile/parser"
+)
+
+var regexDL3007 = regexp.MustCompile(`.*:latest`)
+
+// validateDL3007 Using latest is prone to errors if the image will ever update. Pin the version explicitly to a release tag.
+func validateDL3007(node *parser.Node) (rst []ValidateResult, err error) {
+	for _, child := range node.Children {
+		if child.Value == FROM && regexDL3007.MatchString(child.Next.Value) {
+			rst = append(rst, ValidateResult{line: child.StartLine, addMsg: ""})
+		}
+	}
+	return rst, nil
+}
