94b78d1170c49792ec34a4cc9acc49441a1c3a7b
iamabhishek-dubey <abhishekbhardwaj510@gmail.com>
2019-05-04 15:51:36 +0530

    Added eleventh rule

---
+++ linter/rules/dl3010.go
@@ -0,0 +1,23 @@
+package rules
+
+import (
+	"regexp"
+	"strings"
+
+	"github.com/moby/buildkit/frontend/dockerfile/parser"
+)
+
+var isCompressionExt = regexp.MustCompile(`(?:\.tar\.gz|tar\.bz|\.tar.xz|\.tgz|\.tbz)$`)
+
+// validateDL3010 Use ADD for extracting archives into an image.
+func validateDL3010(node *parser.Node) (rst []ValidateResult, err error) {
+	for _, child := range node.Children {
+		if child.Value == COPY {
+			args := strings.Fields(child.Next.Value)
+			if len(args) >= 1 && isCompressionExt.MatchString(args[0]) {
+				rst = append(rst, ValidateResult{line: child.StartLine, addMsg: ""})
+			}
+		}
+	}
+	return rst, nil
+}
