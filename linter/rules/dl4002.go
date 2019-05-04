e88d3eb7aafe1c621f7378d9954c6f41b29e6e11
iamabhishek-dubey <abhishekbhardwaj510@gmail.com>
2019-05-04 16:08:39 +0530

    Added 29th rule

---
+++ linter/rules/dl4002.go
@@ -0,0 +1,21 @@
+package rules
+
+import (
+	"github.com/moby/buildkit/frontend/dockerfile/parser"
+)
+
+// validateDL4003 Multiple CMD instructions found.
+func validateDL4003(node *parser.Node) (rst []ValidateResult, err error) {
+	isCmd := false
+	for _, child := range node.Children {
+		switch child.Value {
+		case CMD:
+			if !isCmd {
+				isCmd = true
+			} else {
+				rst = append(rst, ValidateResult{line: child.StartLine, addMsg: ""})
+			}
+		}
+	}
+	return rst, nil
+}
