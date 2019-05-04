0361cdbf3101b98cce6987756adf4e5b4e355a4f
iamabhishek-dubey <abhishekbhardwaj510@gmail.com>
2019-05-04 15:49:30 +0530

    Added seventh rule

---
+++ linter/rules/dl3006.go
@@ -0,0 +1,19 @@
+package rules
+
+import (
+	"regexp"
+
+	"github.com/moby/buildkit/frontend/dockerfile/parser"
+)
+
+var regexDL3006 = regexp.MustCompile(`.+[:].+`)
+
+// validateDL3006 Always tag the version of an image explicitly"
+func validateDL3006(node *parser.Node) (rst []ValidateResult, err error) {
+	for _, child := range node.Children {
+		if child.Value == FROM && !regexDL3006.MatchString(child.Next.Value) {
+			rst = append(rst, ValidateResult{line: child.StartLine, addMsg: ""})
+		}
+	}
+	return rst, nil
+}
