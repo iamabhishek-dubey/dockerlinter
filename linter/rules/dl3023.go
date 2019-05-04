b8353bc11b47ffe4cc71543e90a0bedfe34f1eec
iamabhishek-dubey <abhishekbhardwaj510@gmail.com>
2019-05-04 16:04:49 +0530

    Added 24th rule

---
+++ linter/rules/dl3023.go
@@ -0,0 +1,42 @@
+package rules
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+
+	"github.com/moby/buildkit/frontend/dockerfile/parser"
+)
+
+var regexVersion3023 = regexp.MustCompile(`--from=.+`)
+
+// validateDL3023 COPY --from should reference a previously defined FROM alias
+func validateDL3023(node *parser.Node) (rst []ValidateResult, err error) {
+	isAs := false
+	asFromName := ""
+	for _, child := range node.Children {
+		switch child.Value {
+		case FROM:
+			for _, v := range strings.Fields(child.Original) {
+				switch v {
+				case "as":
+					isAs = true
+				default:
+					if isAs {
+						asFromName = v
+						isAs = false
+					} else {
+						asFromName = ""
+					}
+				}
+			}
+		case COPY:
+			for _, v := range strings.Fields(child.Original) {
+				if regexVersion3023.MatchString(v) && v == fmt.Sprintf("--from=%s", asFromName) {
+					rst = append(rst, ValidateResult{line: child.StartLine, addMsg: ""})
+				}
+			}
+		}
+	}
+	return rst, nil
+}
