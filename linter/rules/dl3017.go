2dce620da81a5c35cbc109e42d1ba8699fe88469
iamabhishek-dubey <abhishekbhardwaj510@gmail.com>
2019-05-04 16:01:26 +0530

    Added 18th rule

---
+++ linter/rules/dl3017.go
@@ -0,0 +1,31 @@
+package rules
+
+import (
+	"strings"
+
+	"github.com/moby/buildkit/frontend/dockerfile/parser"
+)
+
+// validateDL3017 Do not use apk upgrade
+func validateDL3017(node *parser.Node) (rst []ValidateResult, err error) {
+	for _, child := range node.Children {
+		if child.Value == RUN {
+			isApk, length := false, len(rst)
+			for _, v := range strings.Fields(child.Next.Value) {
+				switch v {
+				case "apk":
+					isApk = true
+				case "upgrade":
+					if isApk && length == len(rst) {
+						rst = append(rst, ValidateResult{line: child.StartLine, addMsg: ""})
+					}
+				case "&&":
+					isApk = false
+					continue
+				default:
+				}
+			}
+		}
+	}
+	return rst, nil
+}
