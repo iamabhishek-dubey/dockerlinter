2e6c4b8c129465abbe02a954e980f5644536519b
iamabhishek-dubey <abhishekbhardwaj510@gmail.com>
2019-05-04 15:44:26 +0530

    Added first rule

---
+++ linter/rules/dl3000.go
@@ -0,0 +1,23 @@
+package rules
+
+import (
+	"path/filepath"
+
+	"github.com/moby/buildkit/frontend/dockerfile/parser"
+)
+
+// validateDL3000 is "Use absolute WORKDIR."
+func validateDL3000(node *parser.Node) (rst []ValidateResult, err error) {
+	for _, child := range node.Children {
+		if child.Value == WORKDIR {
+			absPath, err := filepath.Abs(child.Next.Value)
+			if err != nil {
+				return nil, err
+			}
+			if absPath != child.Next.Value {
+				rst = append(rst, ValidateResult{line: child.StartLine, addMsg: ""})
+			}
+		}
+	}
+	return rst, nil
+}
