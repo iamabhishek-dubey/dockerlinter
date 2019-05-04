c07cdc2deabf991705ca731a4169955c5b734e3e
iamabhishek-dubey <abhishekbhardwaj510@gmail.com>
2019-05-04 15:51:17 +0530

    Added ninth rule

---
+++ linter/rules/dl3008.go
@@ -0,0 +1,38 @@
+package rules
+
+import (
+	"regexp"
+	"strings"
+
+	"github.com/moby/buildkit/frontend/dockerfile/parser"
+)
+
+var regexDL3008 = regexp.MustCompile(`.+=.+`)
+
+// validateDL3008 Pin versions in apt get install. Instead of `apt-get install <package>` use `apt-get install <package>=<version>`
+func validateDL3008(node *parser.Node) (rst []ValidateResult, err error) {
+	for _, child := range node.Children {
+		if child.Value == RUN {
+			isAptGet, isInstall, length := false, false, len(rst)
+			for _, v := range strings.Fields(child.Next.Value) {
+				switch v {
+				case "apt-get":
+					isAptGet = true
+				case "install":
+					if isAptGet {
+						isInstall = true
+					}
+				case "&&":
+					isAptGet, isInstall = false, false
+					continue
+				default:
+					if isInstall && !regexDL3008.MatchString(v) && length == len(rst) {
+						rst = append(rst, ValidateResult{line: child.StartLine, addMsg: ""})
+						isAptGet, isInstall = false, false
+					}
+				}
+			}
+		}
+	}
+	return rst, nil
+}
