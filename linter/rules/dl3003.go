8ba4b841ba5eea7c8131522e16576b92cd44d63a
iamabhishek-dubey <abhishekbhardwaj510@gmail.com>
2019-05-04 15:46:46 +0530

    Added fourth rule

---
+++ linter/rules/dl3003.go
@@ -0,0 +1,21 @@
+package rules
+
+import (
+	"strings"
+
+	"github.com/moby/buildkit/frontend/dockerfile/parser"
+)
+
+// validateDL3003 is "Use WORKDIR to switch to a directory"
+func validateDL3003(node *parser.Node) (rst []ValidateResult, err error) {
+	for _, child := range node.Children {
+		if child.Value == RUN {
+			for _, v := range strings.Fields(child.Next.Value) {
+				if v == "cd" {
+					rst = append(rst, ValidateResult{line: child.StartLine, addMsg: ""})
+				}
+			}
+		}
+	}
+	return rst, nil
+}
