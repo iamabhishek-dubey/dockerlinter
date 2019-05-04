6a37fa1ae22dc4db37c1d28d38c9232968273048
iamabhishek-dubey <abhishekbhardwaj510@gmail.com>
2019-05-04 15:58:27 +0530

    Added 14th rule

---
+++ linter/rules/dl3013.go
@@ -0,0 +1,37 @@
+package rules
+
+import (
+	"regexp"
+	"strings"
+
+	"github.com/moby/buildkit/frontend/dockerfile/parser"
+)
+
+var regexVersion3013 = regexp.MustCompile(`.+[==|@].+`)
+
+// validateDL3013 Pin versions in pip. Instead of `pip install <package>` use `pip install <package>==<version>`
+func validateDL3013(node *parser.Node) (rst []ValidateResult, err error) {
+	for _, child := range node.Children {
+		if child.Value == RUN {
+			isPip, isInstall, length := false, false, len(rst)
+			for _, v := range strings.Fields(child.Next.Value) {
+				switch v {
+				case "pip":
+					isPip = true
+				case "install":
+					if isPip {
+						isInstall = true
+					}
+				case "&&":
+					isPip, isInstall = false, false
+				default:
+					if isPip && isInstall && !regexVersion3013.MatchString(v) && length == len(rst) {
+						rst = append(rst, ValidateResult{line: child.StartLine, addMsg: ""})
+					}
+					isPip, isInstall = false, false
+				}
+			}
+		}
+	}
+	return rst, nil
+}
