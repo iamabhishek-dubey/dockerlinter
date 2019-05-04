d0ca5f03e01cf3212d16d905995cd2b617b71f34
iamabhishek-dubey <abhishekbhardwaj510@gmail.com>
2019-05-04 16:01:33 +0530

    Added 19th rule

---
+++ linter/rules/dl3018.go
@@ -0,0 +1,37 @@
+package rules
+
+import (
+	"regexp"
+	"strings"
+
+	"github.com/moby/buildkit/frontend/dockerfile/parser"
+)
+
+var regexVersion3018 = regexp.MustCompile(`.+=.+`)
+
+// validateDL3018 Do not use apk upgrade
+func validateDL3018(node *parser.Node) (rst []ValidateResult, err error) {
+	for _, child := range node.Children {
+		if child.Value == RUN {
+			isApk, isAdd, length := false, false, len(rst)
+			for _, v := range strings.Fields(child.Next.Value) {
+				switch v {
+				case "apk":
+					isApk = true
+				case "add":
+					if isApk {
+						isAdd = true
+					}
+				case "&&":
+					isApk, isAdd = false, false
+				default:
+					if isAdd && !regexVersion3018.MatchString(v) && length == len(rst) {
+						rst = append(rst, ValidateResult{line: child.StartLine, addMsg: ""})
+						isApk, isAdd = false, false
+					}
+				}
+			}
+		}
+	}
+	return rst, nil
+}
