7de3286ac302030252b2e6a962a9c9892dc81884
iamabhishek-dubey <abhishekbhardwaj510@gmail.com>
2019-05-04 15:47:26 +0530

    Added fifthh rule

---
+++ linter/rules/dl3004.go
@@ -0,0 +1,21 @@
+package rules
+
+import (
+	"strings"
+
+	"github.com/moby/buildkit/frontend/dockerfile/parser"
+)
+
+// validateDL3004 is "Do not use sudo as it leads to unpredictable behavior. Use a tool like gosu to enforce root."
+func validateDL3004(node *parser.Node) (rst []ValidateResult, err error) {
+	for _, child := range node.Children {
+		if child.Value == RUN {
+			for _, v := range strings.Fields(child.Next.Value) {
+				if v == "sudo" {
+					rst = append(rst, ValidateResult{line: child.StartLine, addMsg: ""})
+				}
+			}
+		}
+	}
+	return rst, nil
+}
