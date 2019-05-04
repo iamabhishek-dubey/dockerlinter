6070935e43e732eddacfe06335399f39961d8370
iamabhishek-dubey <abhishekbhardwaj510@gmail.com>
2019-05-04 15:58:39 +0530

    Added 16th rule

---
+++ linter/rules/dl3015.go
@@ -0,0 +1,34 @@
+package rules
+
+import (
+	"strings"
+
+	"github.com/moby/buildkit/frontend/dockerfile/parser"
+)
+
+// validateDL3015 Avoid additional packages by specifying --no-install-recommends.
+func validateDL3015(node *parser.Node) (rst []ValidateResult, err error) {
+	for _, child := range node.Children {
+		if child.Value == RUN {
+			isAptGet, isInstalled, length := false, false, len(rst)
+			for _, v := range strings.Fields(child.Next.Value) {
+				switch v {
+				case "apt-get":
+					isAptGet = true
+				case "install":
+					if isAptGet {
+						isInstalled = true
+					}
+				case "&&":
+					isAptGet, isInstalled = false, false
+				default:
+					if isInstalled && v != "--no-install-recommends" && length == len(rst) {
+						rst = append(rst, ValidateResult{line: child.StartLine, addMsg: ""})
+					}
+					isAptGet, isInstalled = false, false
+				}
+			}
+		}
+	}
+	return rst, nil
+}
