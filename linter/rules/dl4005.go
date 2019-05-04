efc4287b6401f63cd87ebee6e51c94689d327563
iamabhishek-dubey <abhishekbhardwaj510@gmail.com>
2019-05-04 16:09:04 +0530

    Added 32nd rule

---
+++ linter/rules/dl4005.go
@@ -0,0 +1,28 @@
+package rules
+
+import (
+	"strings"
+
+	"github.com/moby/buildkit/frontend/dockerfile/parser"
+)
+
+// validateDL4006 Set the `SHELL` option -o pipefail before `RUN` with a pipe in it
+func validateDL4006(node *parser.Node) (rst []ValidateResult, err error) {
+	isShellPipeFail := false
+	for _, child := range node.Children {
+		switch child.Value {
+		case SHELL:
+			isShellPipeFail = true
+		case RUN:
+			for _, v := range strings.Fields(child.Next.Value) {
+				switch v {
+				case "|":
+					if !isShellPipeFail {
+						rst = append(rst, ValidateResult{line: child.StartLine, addMsg: ""})
+					}
+				}
+			}
+		}
+	}
+	return rst, nil
+}
